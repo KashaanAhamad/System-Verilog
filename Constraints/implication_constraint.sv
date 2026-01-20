ec6f5d23f38f3ac0c3b7dbd4df5ca3b09d18abf5
Kashaan Ahmad <kashaan.bryan@gmail.com>
2026-01-20 23:56:27 +0530

    Added Implication constraint module

---
+++ Constraints/implication_constraint.sv
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.01.2026 23:52:38
+// Design Name: 
+// Module Name: implication_constraint
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+class ABC;
+  rand bit [2:0] mode;
+  rand bit [3:0] len;
+
+  constraint c_mode { mode == 2 -> len > 10; }
+endclass
+
+module tb;
+  initial begin
+    ABC abc = new;
+    for(int i = 0; i < 10; i++) begin
+      abc.randomize();
+      $display ("mode=%0d len=%0d", abc.mode, abc.len);
+    end
+  end
+endmodule
+
